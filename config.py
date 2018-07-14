071c90cfe06ab8a8e438c73daa775f485a30b0b8
Josh Hoffman <jhoffman2@radford.edugit config --global user.name Josh>
2018-07-14 11:29:22 -0400

    Homework

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "rnQ2Ah3Cm7XoqBsGagWwmf8PA"
+consumer_secret = "4P0MTprjpMPQl2KqDVRd9qiJ8SpuOdCmyIiUL6qjlNUwFUVMiZ"
+access_token = "1466297995-xrZDURxoaw9DcGGPlI5kfrZyVzhHoEXLI4iQRro"
+access_token_secret = "v7IX01Kv3XlLCaCkqjz0AE5YrsZRBbWPKPSMEHN1sV6A3"
